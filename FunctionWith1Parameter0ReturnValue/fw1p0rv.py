e43675152e5e453c1bd5b3bb037a95e43473d27e
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-05-14 23:14:43 +0530

    Create fw1p0rv.py

---
+++ FunctionWith1Parameter0ReturnValue/fw1p0rv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2
+
+def Hello(anyone):              #function with 1 parameter
+        print('Hello ' + anyone)
+
+
+def main():
+	name='Pooja'            #storing a value in a variable...
+        Hello(name)             #giving value to that parameter...
+
+if __name__=='__main__':
+        main()
