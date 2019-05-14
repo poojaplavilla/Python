08e43296e62e3b8a779b44f8d76f7c7ab9e8d38d
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-05-14 23:10:50 +0530

    Create fwnprv.py

---
+++ FunctionWithNOParameter1ReturnValue/fwnprv.py
@@ -0,0 +1,12 @@
+!/usr/bin/python2
+
+def Hello():
+	a='Hello everyone...'
+        return a                #returning value
+
+def main():
+	var=Hello()             #calling function stored in an variable...
+        print(var)              #printing variable
+
+if __name__=='__main__':
+        main()
