a0c1139399e29f3a37d77c1852a743f0149f4dee
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-05-14 23:03:41 +0530

    Create fwna.py

---
+++ TypesOfFunctions/FunctionWithNoArguments/fwna.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2
+
+#function with no arguments...
+def Hello():
+	print("Hello3")
+
+#main function...
+def main():
+	print("Hello2")
+        Hello()                     #Calling Hello() method...
+        print("Hello4")
+if __name__=='__main__':
+        print("Hello1")
+        main()
+	print("Hello5")
+
+#First '__main__' will get executed...
