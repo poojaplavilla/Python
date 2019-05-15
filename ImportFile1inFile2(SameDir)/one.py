b04d7adeba3fa78d0b7e9cbcc158889a43aaa9ca
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-05-15 22:52:28 +0530

    Create one.py

---
+++ ImportFile1inFile2(SameDir)/one.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2
+
+def main():
+      	Hello1()                #calling 1st function
+        Hello2()                #calling 2nd function
+
+def Hello1():
+	print("Hello one...")
+
+def Hello2():
+	print("Hello two...")
+
+if __name__=='__main__':
+        main()
+
