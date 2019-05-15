17f2b85194ca0340dfe8f5a68110d4316e600b28
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-05-15 22:53:46 +0530

    Create two.py

---
+++ ImportFile1inFile2(SameDir)/two.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2
+
+import one                                      #importing prac.py file
+
+def main():
+	      one.Hello1()                           #calling 1st function of 1st file
+        one.Hello2()                           #calling 2nd function of 1st file
+        print("Hello three...")
+
+if __name__=='__main__':
+        main()
+
