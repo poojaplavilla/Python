8da991138c0ddac9df073c9ec84e589abcc48f8f
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-05-15 23:16:14 +0530

    Create fw2a.py

---
+++ FunctionWith2Arguments/fw2a.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2
+
+def fun(var1,var2):
+        result = 'a' + 'a' + 'a'
+        return result;
+def main():
+	var1=input('Enter 1st value: ')
+        var2=input('Enter 2nd value: ')
+
+        output=fun(var1,var2)
+        print(output)
+if __name__=='__main__':
+        main()
