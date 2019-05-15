cea24fad5d26a76829d8462522d52aeccb162e0e
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-05-15 23:35:56 +0530

    Update fwna.py

--- FunctionWithNoArguments/fwna.py
+++ FunctionWithNoArguments/fwna.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python2
 
 #function with no arguments...
+print("Hello1")
 def Hello():
-	print("Hello3")
-
+	print("Hello6")
+print("Hello2")
 #main function...
+
 def main():
-	print("Hello2")
-        Hello()                     #Calling Hello() method...
-        print("Hello4")
-if __name__=='__main__':
-        print("Hello1")
-        main()
 	print("Hello5")
+        Hello()                     #Calling Hello() method...
+print("Hello3")
+
+if __name__=='__main__':
+        print("Hello4")
+        main()
+	print("Hello7")
 
 #First '__main__' will get executed...
